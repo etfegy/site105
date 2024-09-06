64af8ddf50b6013950cc21120b4157cc417ad0b2
etfegy <75904355+etfegy@users.noreply.github.com>
2024-09-06 20:22:28 +0400

    Delete content directory

--- content/js/main.js
+++
@@ -1,7 +0,0 @@
-const icon = document.querySelector('.icon');
-const nav = document.querySelector('nav');
-
-icon.addEventListener('click', () => {
-  icon.classList.toggle('close');
-  nav.classList.toggle('show');
-})
